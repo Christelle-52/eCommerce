a12ca0a39bb3838708b97bc9cdfeec5cad4a9508
Christelle-52 <christelle.billon52@gmail.com>
2023-09-16 15:56:22 +0200

    modif eye.js en if + ajout class erreur et valide sur form.css

--- eye.js
+++ eye.js
@@ -2,30 +2,21 @@ let eyeOn = document.querySelectorAll('.feather-eye');
 let eyeOff = document.querySelectorAll('.feather-eye-off');
 let passwordField = document.querySelectorAll("input[type=password]");
 
-eyeOn[0].addEventListener('click', (e) => {
-  e.preventDefault();
-  eyeOn[0].style.display = "none";
-  eyeOff[0].style.display = "block";
-  passwordField[0].type = "text";
-});
+for (let i = 0; i < eyeOn.length; i++) {
+  eyeOn[i].addEventListener('click', (e) => {
+    e.preventDefault();
+    eyeOn[i].style.display = "none";
+    eyeOff[i].style.display = "block";
+    passwordField[i].type = "text";
+  });
+}
 
-eyeOff[0].addEventListener('click', (e) => {
-  e.preventDefault();
-  eyeOff[0].style.display = "none";
-  eyeOn[0].style.display = "block";
-  passwordField[0].type = "password";
-});
+for (let i = 0; i < eyeOn.length; i++) {
+  eyeOff[i].addEventListener('click', (e) => {
+    e.preventDefault();
+    eyeOff[i].style.display = "none";
+    eyeOn[i].style.display = "block";
+    passwordField[i].type = "password";
+  });
+}
 
-eyeOn[1].addEventListener('click', (e) => {
-  e.preventDefault();
-  eyeOn[1].style.display = "none";
-  eyeOff[1].style.display = "block";
-  passwordField[1].type = "text";
-});
-
-eyeOff[1].addEventListener('click', (e) => {
-  e.preventDefault();
-  eyeOff[1].style.display = "none";
-  eyeOn[1].style.display = "block";
-  passwordField[1].type = "password";
-});
